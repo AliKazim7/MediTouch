431f67406971320664e92cb21f43aa1a3716ec47
AliKazim7 <aghaalikazim50@gmail.com>
2020-01-08 22:18:18 +0500

    First push

---
+++ src/Main.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
+import { Root } from 'native-base';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+
+
+// Our custom files and classes import
+import Home from './page/Home';
+// import Search from './page/Search';
+// import Cart from './page/Cart';
+// import WishList from './page/WishList';
+// import Map from './page/Map';
+// import Newsletter from './page/Newsletter';
+// import Contact from './page/Contact';
+// import Category from './page/Category';
+// import Product from './page/Product';
+// import ImageGallery from './page/ImageGallery';
+import Login from './page/Login';
+import Signup from './page/Signup';
+// import Checkout from './page/Checkout';
+
+export default class Main extends Component {
+  componentWillMount = () => {
+    BackHandler.addEventListener('hardwareBackPress', () => Actions.pop());
+  };
+
+  render() {
+    return(
+      <Root>
+        <Router>
+          <Scene key="root">
+            <Scene key="home" component={Home} hideNavBar />
+            <Scene key="login" component={Login} hideNavBar />
+            <Scene initial key="signup" component={Signup} hideNavBar />
+          </Scene>
+        </Router>
+      </Root>
+    );
+  }
+
+}
+
+
+// <Scene initial key="home" component={Home} hideNavBar />
+// <Scene key="search" component={Search} modal hideNavBar />
+// <Scene key="cart" component={Cart} modal hideNavBar />
+// <Scene key="wishlist" component={WishList} modal hideNavBar />
+// <Scene key="contact" component={Contact} modal hideNavBar />
+// <Scene key="newsletter" component={Newsletter} modal hideNavBar />
+// <Scene key="category" component={Category} hideNavBar />
+// <Scene key="product" component={Product} hideNavBar />
+// <Scene key="login" component={Login} hideNavBar />
+// <Scene key="signup" component={Signup} hideNavBar />
+// <Scene key="checkout" component={Checkout} hideNavBar />
